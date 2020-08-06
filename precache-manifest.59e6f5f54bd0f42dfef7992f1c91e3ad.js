2217632f6e54fdd2b135d449f88cd0bc55700da7
Viktoria Hetman <pvp071984@gmail.com>
2020-08-06 17:48:36 +0300

    Updates

---
+++ precache-manifest.59e6f5f54bd0f42dfef7992f1c91e3ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b6ef9202e6e1a1042e69c7c2b44468",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "5aec9fc754a15c8ce314",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.6c2b900d.chunk.css"
+  },
+  {
+    "revision": "1773d73e8e6485992bb5",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.92860add.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.92860add.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aec9fc754a15c8ce314",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.5bd2a159.chunk.js"
+  },
+  {
+    "revision": "356906f07e3d724d1528",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.d720b385.js"
+  }
+]);
